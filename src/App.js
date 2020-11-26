043db32f051abf683486860de977ba24c93a4630
kaniGupta <kanishkgupta13@gmail.com>
2020-11-26 16:21:36 +0530

    added card component to display.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      monsters: [
-        { name: 'Frankenstein', id: '1' },
-        { name: 'Dracula', id: '2' },
-        { name: 'Zombie', id: '3' },
-      ],
-    };
+    this.state = { monsters: [] };
+  }
+
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
